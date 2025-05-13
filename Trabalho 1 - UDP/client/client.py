948e3f14f716b383a3114b3c6cd520f6aa4e33a0
Gabriel Cassol Bach <gbach@MMB039579CWB.local>
2025-05-13 20:28:08 -0300

    feat: client ready

--- Trabalho 1 - UDP/client/client.py
+++ Trabalho 1 - UDP/client/client.py
@@ -5,8 +5,11 @@ import os
 import hashlib
 import random
 
+SERVER_IP='192.168.0.147'
+SERVER_PORT=6363
+
 class client:
-    def __init__(self, host='localhost', port=6363):
+    def __init__(self, host=SERVER_IP, port=SERVER_PORT):
         self.host = host
         self.port = port
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -23,7 +26,7 @@ class client:
         print(f"Arquivo {file_name} recebido com sucesso.")
 
     def _receive_file(self, file_name):
-        data, _ = self.server_socket.recvfrom(8)
+        data, _ = self.server_socket.recvfrom(1024)
         total_size = struct.unpack('!Q', data)[0] #total_size = 13.
         bytes_counter = 0
         #print(f"Tamanho total do arquivo: {total_size} bytes")
@@ -31,9 +34,9 @@ class client:
         with open(file_name, 'wb') as f:
             
             while bytes_counter < total_size:
-                packet, _ = self.server_socket.recvfrom(8+8+32) # 40 bytes do header(4 bytes seq, 4 bytes tam. pacote, 32 bytes hash) + 8 bytes dos dados do pacote
+                packet, _ = self.server_socket.recvfrom(1024) # 40 bytes do header(4 bytes seq, 4 bytes tam. pacote, 32 bytes hash) + 8 bytes dos dados do pacote
                 seq, bytes_length, hash = struct.unpack('!II32s', packet[:40])
-                data = packet[40:40+bytes_length]
+                data = packet[40:]
                 
                 # Hash verification
                 calculated_hash = hashlib.sha256(data).digest()
@@ -47,16 +50,16 @@ class client:
                 self.server_socket.sendto(ack, (self.host, self.port))
 
     def _debug_receive_file(self, file_name):
-        data, _ = self.server_socket.recvfrom(8)
+        data, _ = self.server_socket.recvfrom(1024)
         total_size = struct.unpack('!Q', data)[0] #total_size = 13.
         bytes_counter = 0
-        random_seq = random.randint(0, int(total_size/8) - 1) # Escolher um número aleatório entre 0 e total_size/8 - 1 para simular perda de pacotes
+        random_seq = random.randint(0, int(total_size/512) - 1) # Escolher um número aleatório entre 0 e total_size/8 - 1 para simular perda de pacotes
         #print(f"Tamanho total do arquivo: {total_size} bytes")
 
         with open(file_name, 'wb') as f:
             
             while bytes_counter < total_size:
-                packet, _ = self.server_socket.recvfrom(8+8+32) # 40 bytes do header(4 bytes seq, 4 bytes tam. pacote, 32 bytes hash) + 8 bytes dos dados do pacote
+                packet, _ = self.server_socket.recvfrom(1024) # 40 bytes do header(4 bytes seq, 4 bytes tam. pacote, 32 bytes hash) + 8 bytes dos dados do pacote
                 seq, bytes_length, hash = struct.unpack('!II32s', packet[:40])
                 data = packet[40:40+bytes_length]
 
@@ -82,14 +85,10 @@ def main():
     if len(sys.argv) != 2:
         print("Uso: python client.py <nome_do_arquivo>")
         sys.exit(1)
-    file_name = sys.argv[1]
-    if not os.path.isfile(file_name):
-        print(f"Arquivo {file_name} não encontrado.")
-        sys.exit(1)
 
     file_name = sys.argv[1]
     client_instance = client()
-    client_instance.request_file(file_name, debug=True) # Modo de simulação de perda ativado
+    client_instance.request_file(file_name, debug=False) # Modo de simulação de perda ativado
 
 if __name__ == "__main__":
     main()
